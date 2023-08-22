58c7409306fb991435349dbe036b18f1bb6f3030
Default540 <jv6542436@outlook.com>
2023-08-22 02:35:55 -0300

    'd'

--- L3/E1.java
+++ L3/E1.java
@@ -1,7 +1,10 @@
 package L3;
 
+import java.util.Scanner;
+
 public class E1 {
     public static void main(String[] args) {
-        
+        final Scanner in = new Scanner(System.in); 
+        in.close();
     }
 }

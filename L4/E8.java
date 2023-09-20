3fe3a0489b1c881eb6342ec01d2b286af45d6b21
Default540 <80178842+Default540@users.noreply.github.com>
2023-09-19 22:15:41 -0300

    ''

--- L4/E8.java
+++ L4/E8.java
@@ -1,4 +1,8 @@
 package L4;
+
+import java.util.Scanner;
+
+//package L4;
 /*
  * Uma loja está levantando o valor total de todas as mercadorias em estoque. Escreva                           
 um algoritmo que permita a entrada das seguintes informações: 
@@ -8,7 +12,25 @@ Ao final imprimir o valor total em estoque e a média dos valores das mercadoria
  */
 public class E8 {
     public static void main(String[] args) {
-       
         
+        final Scanner in = new Scanner(System.in);
+        
+        int quantMercadorias = in.nextInt();
+
+        float totMercadorias = 0;
+        
+
+        int count = 0;
+        while (count < quantMercadorias) {
+            System.out.println("quanto custa a mercadoria "+(count+1));
+            float preco = in.nextFloat();
+            totMercadorias+=preco;
+            count++;
+        }
+        
+        float mediaValores = totMercadorias/quantMercadorias;
+        System.out.println("Total de Mercadorias: "+totMercadorias+"\nMedia de mercadorias: "+mediaValores);
+
+        in.close();
     }
 }
